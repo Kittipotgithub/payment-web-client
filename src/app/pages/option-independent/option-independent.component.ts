77be56406ca17b278a36784ce0d3a36e4fb598ec
gnkktp <nattawanktp@gmail.com>
2020-01-22 23:00:45 +0700

    independent_option_content

--- src/app/pages/option-independent/option-independent.component.ts
+++ src/app/pages/option-independent/option-independent.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-option-independent',
   templateUrl: './option-independent.component.html',
   styleUrls: ['./option-independent.component.scss']
 })
 export class OptionIndependentComponent implements OnInit {
+  
+  foods: Food[] = [
+    {value: '1', viewValue: 'เลขที่เอกสาร'},
+    {value: '2', viewValue: 'ประเภทเอกสาร'},
+    {value: '3', viewValue: 'วันผ่านรายการ'},
+    {value: '4', viewValue: 'การอ้างอิง'},
+    {value: '5', viewValue: 'แหล่งของเงิน'},
+    {value: '6', viewValue: 'วิธีการชำระเงิน'},
+    {value: '7', viewValue: 'การระงับการชำระเงิน'},
+    {value: '8', viewValue: 'อื่นๆ'}
+  ];
 
   constructor() { }
 
