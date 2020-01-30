5e8cb75c3939b7b09ef247a7e0a803ebe2a72b44
kittipot.phe <kittipot.phe@gmail.com>
2020-01-30 22:52:26 +0700

    fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogSearchVendorComponent } from './component/tab-param/dialog-search-vendor/dialog-search-vendor.component';
-import { MatDialogModule, MatTabsModule, MatDatepickerModule, MatNativeDateModule, MatTooltipModule, MatProgressBarModule, MatCheckboxModule, MatSelectModule, MatTableModule, MatFormFieldModule, MatInputModule, MatIconModule, MatToolbarModule, MatCardModule, MatAutocompleteModule, MatRadioModule } from '@angular/material';
+import { MatDialogModule, MatTabsModule, MatDatepickerModule, MatNativeDateModule, MatTooltipModule, MatProgressBarModule, MatCheckboxModule, MatSelectModule, MatTableModule, MatFormFieldModule, MatInputModule, MatIconModule, MatToolbarModule, MatCardModule, MatAutocompleteModule, MatRadioModule, MatSortModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -56,7 +56,8 @@ import { DialogSearchVendorPaymentComponent } from './component/dialog-search-ve
     DragDropModule,
     FormsModule,
     ScrollingModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSortModule
   ],
   entryComponents:[
     DialogSearchVendorComponent,
