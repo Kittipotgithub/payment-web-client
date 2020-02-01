10b6e045ceb4fd8f6e7999dc76b9e34465ef42fd
phudis <phudis.rt@gmail.com>
2020-02-02 02:03:06 +0700

    tab independent

---
+++ src/app/core/service/payment-independent/payment-independent.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { map, take, catchError } from 'rxjs/operators';
+import { ApiService } from '../../api.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaymentIndependentService {
+  constructor(private httpClient: HttpClient) {}
+
+
+  search( value): Promise<any> {
+    return this.httpClient
+      .get(`${environment.apiUrl}` + '/paymentIndependentConfig/searchGroup/' + value )
+      .pipe(
+        map(data => {
+          console.log(data);
+          return data;
+        }),
+        catchError(err => {
+          console.log(err);
+          return of(err);
+        }),
+        take(1)
+      )
+      .toPromise();
+  }
+
+  searchStandard(): Promise<any> {
+    return this.httpClient
+      .get(`${environment.apiUrl}` + '/paymentIndependentConfig/searchStandard')
+      .pipe(
+        map(data => {
+          console.log(data);
+          return data;
+        }),
+        catchError(err => {
+          console.log(err);
+          return of(err);
+        }),
+        take(1)
+      )
+      .toPromise();
+  }
+}
